adf01272f4851aaac1f3ec20c189fe2405c240ac
dcbozada <dcbozada@gmail.com>
2025-11-10 20:03:52 -0600

    hitting the user's saved tracks endpoint, success

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import requests
+import requests # type: ignore
+import json
 from get_token import get_code_verifier, get_code_challenge, create_oauth_link, get_token
 
 def main():#
@@ -6,9 +7,12 @@ def main():#
     print(f"Please use following link to grant access to your Spotify data {oauth_link}")
     access_token = get_token(verifier)
     print(f"Your access token: {access_token}")
-    url = "https://api.spotify.com/v1/me/tracks"
+    url = "https://api.spotify.com/v1/me/tracks?limit=20"
+    headers={f"Authorization": f"Bearer {access_token}"}
     
-    my_tracks = requests.get()
+    my_tracks = requests.get(url, headers=headers)
+    my_tracks = my_tracks.json()
+    print(my_tracks)
 
 if __name__ == "__main__":
     main()
