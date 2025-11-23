5171e2acb36ac3495fdc3f762ea4e372536a30ca
dcbozada <dcbozada@gmail.com>
2025-11-23 12:20:41 -0600

    created database postgreSQL spotify, created table 'tracks', and inserted tracks_df into table tracks

--- etl.py
+++ etl.py
@@ -39,8 +39,49 @@ def main():
     # connect to local postgres DB
     engine = create_engine("postgresql://dylan:wooli@localhost:5432/spotify")
 
+    # testing connection to database 'spotify'
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text('SELECT 1'))
+            print("Successful connection to 'spotify': ", result.scalar())
+    except Exception as e:
+        print("Failed Connection to 'spotify'", e)
 
-    # tracks_df.to_sql("tracks", engine, if_exists="replace", index=False)
+    # creating the table 'tracks'
+    create_tracks_table_sql = """
+    CREATE TABLE IF NOT EXISTS tracks (
+    track_id VARCHAR PRIMARY KEY,
+    track_name VARCHAR,
+    artist_id VARCHAR, 
+    album_id VARCHAR, 
+    duration_ms INT 
+    );"""
+    with engine.connect() as conn:
+        conn.execute(text(create_tracks_table_sql))
+        conn.commit()
+    print("Table 'tracks' created")
+
+    # inserting tracks_df into table 'tracks'
+    # no need to commit because sqlalchemy to_sql() is in autocommit mode
+    # opens temp connection, executes the INSERT statements, auto commits, closes connection
+    tracks_df.to_sql("tracks", engine, if_exists='replace', index=False)
+
+    # testing to see if tracks loaded
+    with engine.connect() as conn:
+        # using engine.connect().execute()
+        # yields sqlalchemy CursorResult object
+        # that needs to printed with a "for row in result" as if you were trying
+        # view a "SELECT * FROM tracks"
+        result = conn.execute(text("SELECT COUNT(*) FROM tracks"))
+        for row in result:
+            print(row)
+        # using "pd.read_sql(query, connection)" reads the result into a 
+        # pandas DataFrame
+        test = pd.read_sql("SELECT COUNT(*) FROM tracks", engine)
+        print(test)
+
+
+
 
 
 
