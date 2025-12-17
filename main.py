f69c2d7bcd29ff343de33f186051f738ee4a658a
dcbozada <dcbozada@gmail.com>
2025-12-17 10:10:46 -0600

    incorporating .gitignore file for api key safety

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from etl import ETL
 TRACKS = "tracks.json"
 ARTISTS = "artists.json"
 ALBUMS = "albums.json"
+TOKEN_DATA = "token_data.json"
 
 def main():
     # create instances of necessary classes
@@ -17,7 +18,7 @@ def main():
     oauth_link, verifier = token.create_oauth_link()
     print(f"Please use following link to grant access to your Spotify data {oauth_link}")
     access_token = token.get_token(verifier)
-    print(f"Your access token: {access_token}")
+    print(f"\nYour access token: {access_token}")
 
     # headers dictionary I will be using when making GET requests to api endpoints
     headers={f"Authorization": f"Bearer {access_token}"}
--- test.py
+++ test.py
@@ -1,10 +1,29 @@
 import pandas as pd
 import json
+import requests
+from etl import ETL
+from get_token import Token
 
-with open('artists.json', 'r') as f:
-    artists = json.load(f)
-genre = (artists['artists'][5].get('genres') or ['No Genre specified'])[0]
-print(genre)
-print(type(artists))
-print(type(artists['artists']))
-print(type(artists['artists'][6]))
+TRACKS = "tracks.json"
+
+etl = ETL()
+token = Token()
+
+access_token = token.get_access_token()
+print(access_token)
+
+# # headers dictionary I will be using when making GET requests to api endpoints
+# headers={f"Authorization": f"Bearer {access_token}"}
+
+# # url is to get user 50 tracks from users liked songs playlists, only first 50
+# my_tracks_url = "https://api.spotify.com/v1/me/tracks?limit=50"
+
+# # request tracks info with GET and turn result into JSON
+# result = requests.get(my_tracks_url, headers=headers)
+# if result.status_code == 401:
+#     token.refresh_ac
+# print(type(result))
+# tracks_df = etl.jsonToDf(file_name=TRACKS, proc_what='tracks',
+#                             result=result)
+# for i in tracks_df.columns:
+#     print(i)
