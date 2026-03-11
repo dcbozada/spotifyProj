c39a07085a513fd2c4b24693fad630cbba3614e7
dcbozada <dcbozada@gmail.com>
2026-03-10 23:44:29 -0400

    added api.py to keep main.py clean, it handles all requests to spotify end points

---
+++ api.py
@@ -0,0 +1,84 @@
+import requests # type: ignore
+import pandas as pd
+from get_token import Token
+from etl import ETL
+
+# setting json file names as global constants
+HISTORY = "listening_history.json"
+TRACKS = "tracks.json"
+ARTISTS = "artists.json"
+ALBUMS = "albums.json"
+
+class Spotify():
+    def __init__(self):
+        self.token = Token()
+        self.etl = ETL()
+        self.access_token = self.token.get_token()
+        self.headers = {f"Authorization": f"Bearer {self.access_token}"}
+
+    def get_history(self):
+        # url is to get users 50 most recently listed to tracks - the engine of database
+        listening_history_url = "https://api.spotify.com/v1/me/player/recently-played?limit=50"
+
+        # request listening info with GET and turn result in JSON
+        result = requests.get(listening_history_url, headers=self.headers)
+        history_df = self.etl.jsonToDf(file_name=HISTORY, proc_what='history',
+                                result=result)
+        return history_df
+    
+    def get_my_tracks(self):
+        # url is to get user 50 tracks from users liked songs playlists, only first 50
+        my_tracks_url = "https://api.spotify.com/v1/me/tracks?limit=50"
+        
+        # request tracks info with GET and turn result into JSON
+        result = requests.get(my_tracks_url, headers=self.headers)
+        tracks_df = self.etl.jsonToDf(file_name=TRACKS, proc_what='tracks',
+                             result=result)
+        return tracks_df
+    
+    def get_artists(self, tracks_df: pd.DataFrame):
+        # empty string to append to for when I pull multiple artists
+        artist_ids = ""
+        for i in tracks_df.index:
+            artist_ids += f'{tracks_df.loc[i, 'artist_id']},'
+        # seting string to exclude the last character(it ends with a comma)
+        artist_ids = artist_ids[:-1]
+        
+        # request artists info with GET and turn results into JSON
+        artist_url = f"https://api.spotify.com/v1/artists?ids={artist_ids}"
+        result = requests.get(artist_url, headers=self.headers)
+        artists_df = self.etl.jsonToDf(file_name = ARTISTS, proc_what='artists',
+                                result=result)
+        return artists_df
+    
+    def get_albums(self, tracks_df:pd.DataFrame):
+        # empty string to append to for when I pull multiple albums
+        album_ids_1 = ""
+        album_ids_2 = ""
+        album_ids_3 = ""
+        for i in tracks_df.index:
+            if i > 0 and i <= 19:
+                album_ids_1 += f"{tracks_df.loc[i, 'album_id']},"
+            elif i >= 20 and i <= 39:
+                album_ids_2 += f"{tracks_df.loc[i, 'album_id']},"
+            else:
+                album_ids_3 += f"{tracks_df.loc[i, 'album_id']},"
+        album_ids_1 = album_ids_1[:-1]
+        album_ids_2 = album_ids_2[:-1]
+        album_ids_3 = album_ids_3[:-1]
+        # request album endpoint and turn JSON to df
+        # we have to append the results and make multiple calls due to 20 LIMIT max 
+        album_url_1 = f"https://api.spotify.com/v1/albums?ids={album_ids_1}"
+        album_url_2 = f"https://api.spotify.com/v1/albums?ids={album_ids_2}"
+        album_url_3 = f"https://api.spotify.com/v1/albums?ids={album_ids_3}"
+        result_1 = requests.get(album_url_1,headers=self.headers)
+        result_2 = requests.get(album_url_2,headers=self.headers)
+        result_3 = requests.get(album_url_3,headers=self.headers)
+        album_df = pd.concat(
+            [
+                self.etl.jsonToDf(file_name=ALBUMS, proc_what='albums', result=result_1),
+                self.etl.jsonToDf(file_name=ALBUMS, proc_what='albums', result=result_2),
+                self.etl.jsonToDf(file_name=ALBUMS, proc_what='albums', result=result_3),
+            ]
+        )
+        return album_df
--- main.py
+++ main.py
@@ -1,83 +1,15 @@
-import requests # type: ignore
-import json
-from get_token import Token
-from etl import ETL
-
-# setting json file names as global constants
-HISTORY = "listening_history.json"
-TRACKS = "tracks.json"
-ARTISTS = "artists.json"
-ALBUMS = "albums.json"
+from api import Spotify
 
 def main():
-    # create instances of necessary classes
-    token = Token()
-    etl = ETL()
-
-    access_token = token.get_token()
-    print(f"\nYour Access Token is: {access_token}")
-
-    # headers dictionary I will be using when making GET requests to api endpoints
-    headers={f"Authorization": f"Bearer {access_token}"}
-
-    # url is to get users 50 most recently listed to tracks - the engine of database
-    listening_history_url = "https://api.spotify.com/v1/me/player/recently-played?limit=50"
-
-
-    # request listening info with GET and turn result in JSON
-    result = requests.get(listening_history_url, headers=headers)
-    history_df = etl.jsonToDf(file_name=HISTORY, proc_what='history',
-                              result=result)
+    spotify = Spotify()
+    history_df = spotify.get_history()
     print(history_df.head())
-
-
-    # url is to get user 50 tracks from users liked songs playlists, only first 50
-    my_tracks_url = "https://api.spotify.com/v1/me/tracks?limit=50"
-    
-    # request tracks info with GET and turn result into JSON
-    result = requests.get(my_tracks_url, headers=headers)
-    tracks_df = etl.jsonToDf(file_name=TRACKS, proc_what='tracks',
-                             result=result)
+    tracks_df = spotify.get_my_tracks()
     print(tracks_df.head())
-
-    # print(tracks_df)
-
-    # empty string to append to for when I pull multiple artists
-    artist_ids = ""
-    for i in tracks_df.index:
-        artist_ids += f'{tracks_df.loc[i, 'artist_id']},'
-    # seting string to exclude the last character(it ends with a comma)
-    artist_ids = artist_ids[:-1]
-    
-    # request artists info with GET and turn results into JSON
-    artist_url = f"https://api.spotify.com/v1/artists?ids={artist_ids}"
-    result = requests.get(artist_url, headers=headers)
-    artists_df = etl.jsonToDf(file_name = ARTISTS, proc_what='artists',
-                               result=result)
+    artists_df = spotify.get_artists(tracks_df=tracks_df)
     print(artists_df.head())
-
-    # empty string to append to for when I pull multiple albums
-    album_ids_1 = ""
-    album_ids_2 = ""
-    album_ids_3 = ""
-    for i in tracks_df.index:
-        if i > 0 and i <= 19:
-            album_ids_1 += f"{tracks_df.loc[i, 'album_id']},"
-        elif i >= 20 and i <= 39:
-            album_ids_2 += f"{tracks_df.loc[i, 'album_id']},"
-        else:
-            album_ids_3 += f"{tracks_df.loc[i, 'album_id']},"
-    album_ids_1 = album_ids_1[:-1]
-    album_ids_2 = album_ids_2[:-1]
-    album_ids_3 = album_ids_3[:-1]
-
-    # request album endpoint and turn JSON to df
-    # we have to append the results and make multiple calls due to 20 LIMIT max 
-    album_url = f"https://api.spotify.com/v1/albums?ids={album_ids_1}"
-    result = requests.get(album_url,headers=headers)
-    album_df = etl.jsonToDf(file_name=ALBUMS, proc_what='albums',
-                            result=result)
-    print(album_df.head())
+    albums_df = spotify.get_albums(tracks_df=tracks_df)
+    print(albums_df.head())
  
 if __name__ == "__main__":
     main()
